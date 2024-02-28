dfe984f99554018084f746b7cff8f323384601dc
tarunsaini <tarunsaini532@gmail.com>
2024-02-29 00:14:52 +0530

    added the code

---
+++ script.js
@@ -0,0 +1,9 @@
+var sharebutton = document.querySelector(".shareButton");
+var sharebutton1 = document.querySelector(".shareButton1");
+
+function shareMenuToggle() {
+  sharebutton.classList.toggle("shareButtonHeight");
+}
+function shareMenuToggle1() {
+  sharebutton1.classList.toggle("shareButton1Height");
+}
